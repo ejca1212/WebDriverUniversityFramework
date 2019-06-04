16f73e13a8863e3428256b9097fe6e652dc0186d
ejca1212 <ecastillo@hugeinc.com>
2019-06-04 15:07:44 -0500

    Added new options for ChromeDriver

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,7 +1,9 @@
 package utils;
 
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -44,9 +46,23 @@ public class DriverFactory {
                         // Specify which driver will be used
                         System.setProperty("webdriver.chrome.driver", os);
 
-                        // Create a new driver object
-                        driver = new ChromeDriver();
+
+
                         //driver.manage().window().maximize();
+                        ChromeOptions options = new ChromeOptions();
+
+                        options.addArguments("enable-automation");
+                        options.addArguments("--headless");
+                        options.addArguments("--window-size=1920,1080");
+                        options.addArguments("--no-sandbox");
+                        options.addArguments("--disable-extensions");
+                        options.addArguments("--dns-prefetch-disable");
+                        options.addArguments("--disable-gpu");
+                        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+                        // Create a new driver object
+                        driver = new ChromeDriver(options);
+
                         break;
 
                     case "ie":
